dda7a8a796f3eb08651e29a16faf24bb91b318ac
João Victor <jvok44020406@gmail.com>
2026-09-18 21:22:31 -0300

    Adicionar comandos pela URL

--- src/main/java/com/jvzin/controle_servidor/ComandoController.java
+++ src/main/java/com/jvzin/controle_servidor/ComandoController.java
@@ -1,29 +1,16 @@
-package com.jvzin.controle_servidor;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-public class ComandoController {
-
-    private String ultimoComando = "";
-
-    @PostMapping("/comando")
-    public String enviarComando(
-            @RequestParam String comando
-    ) {
-
-        ultimoComando = comando;
-
+package com.jvzin.controle_servidor; import 
+org.springframework.web.bind.annotation.*; @RestController public class 
+ComandoController {
+    private String ultimoComando = ""; @PostMapping("/comando") public 
+    String enviarComando(
+            @RequestParam String comando ) { ultimoComando = comando; 
         return "Comando recebido: " + comando;
     }
-
-    @GetMapping("/comando")
-    public String obterComando() {
-
-        String comando = ultimoComando;
-
-        ultimoComando = "";
-
-        return comando;
+    @GetMapping("/comando") public String obterComando() { String 
+        comando = ultimoComando; ultimoComando = ""; return comando;
+    }
+    @GetMapping("/enviar") public String enviarComandoPelaUrl( 
+            @RequestParam String comando
+    ) { ultimoComando = comando; return "Comando enviado: " + comando;
     }
 }
